68f2bce1519d6b63f76156d1024730ffdecb7452
Abdelrahman Mansour741 <abdo.mansour741@gmail.com>
2025-11-03 20:55:26 +0200

    MVC-Session08

--- Company.Pro.PL/Program.cs
+++ Company.Pro.PL/Program.cs
@@ -72,7 +72,7 @@ namespace Company.Pro.PL
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Department}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
